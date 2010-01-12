22f0c8152495a3bc7e6e85dd0407dc38f6087efd
Jonas Haag <jonas@lophus.org>
2010-01-12 19:18:40 +0100

    First try of a configuration backend rewrite.

--- config/backend.py
+++ config/backend.py
@@ -1,33 +1,102 @@
 import os
-from gpyconf.backends import Backend
+
 try:
     from xml.etree import cElementTree as elementtree
 except ImportError:
     from xml.etree import  ElementTree as elementtree
+from xml.parsers.expat import ExpatError
 
+from gpyconf.backends import Backend
+from gpyconf.backends._xml.xmlserialize import to_python_object, unserialize, serialize
+from cream.config import fields
 from cream.util.string import slugify
 
+
+CONFIGURATION_DIRECTORY   = 'configuration'
 STATIC_OPTIONS_FILE       = 'static-options.xml'
 CONFIGURATION_SCHEMA_FILE = 'configuration.xml'
+PROFILE_ROOT_NODE         = 'configuration_profile'
+STATIC_OPTIONS_ROOT_NODE  = 'static_options'
+
+NONFLAT_FIELDS = ('dict', 'list', 'tuple') # opposite of "flat"?
+
+GPYCONF_FIELDS_LOWERCASED = dict((name.lower()[:-5], getattr(fields, name))
+                                 for name in fields.__all__)
+TYPE_ALIASES = {
+    'str' : 'char',
+    'int' : 'integer'
+}
+
+
+def field_for_type(type):
+    return GPYCONF_FIELDS_LOWERCASED[TYPE_ALIASES.get(type, type)]
 
 class CreamXMLBackend(dict, Backend):
-    def __init__(self, profiles_folder):
+    def __init__(self, directory='.'):
+        Backend.__init__(self, None)
         dict.__init__(self)
-        self.profiles_folder = self.profiles_folder
+        self.directory = directory
+        self.configuration_dir = os.path.join(self.directory,
+                                              CONFIGURATION_DIRECTORY)
 
-    def get_option(self, item):
-        try:
-            return self.__getitem__(item)
-        except KeyError:
-            raise MissingOption(item)
+    def read_scheme(self):
+        field_scheme = {}
 
-    set_option = dict.__setitem__
+        xmltree = elementtree.parse(os.path.join(self.directory,
+                                                 CONFIGURATION_SCHEMA_FILE))
+        root_node = xmltree.getroot()
+        for node in root_node.getchildren():
+            option_name = node.tag
+            attributes = node.attrib
+            field_type = attributes.pop('field')
+            if field_type in NONFLAT_FIELDS:
+                attributes['default'] = to_python_object(node, field_type)
+            else:
+                attributes.setdefault('default', node.text)
+            field = field_for_type(field_type)(**attributes)
 
-    options = dict.keys
-    tree = property(lambda self:self)
+            field_scheme[option_name] = field
+
+        return field_scheme
 
     def read(self):
-        pass
+        if not os.path.exists(self.configuration_dir):
+            return dict(), tuple()
+
+        static_options = {}
+        profiles = []
+
+        for profile in os.listdir(self.configuration_dir):
+            with open(os.path.join(self.configuration_dir, profile)) as f:
+                if profile == STATIC_OPTIONS_FILE:
+                    static_options.update(unserialize(f))
+                    continue
+                try:
+                    profile = unserialize(f)
+                except ExpatError, e:
+                    self.warn("Failed to parse '%s': %s" % (fobj.name, e))
+                else:
+                    profiles.append(profile)
+
+        return static_options, profiles
 
     def save(self, profile_list, fields):
-        pass
+        if not os.path.exists(self.configuration_dir):
+            os.makedirs(self.configuration_dir)
+
+        for profile in (p for p in profile_list if p.is_editable):
+            with open(os.path.join(self.configuration_dir,
+                                   slugify(profile.name)+'.xml'), 'w') as fobj:
+                serialize({
+                    'name' : profile.name,
+                    'values' : profile.values,
+                    'position' : profile_list.index(profile),
+                    'selected' : profile_list.active == profile
+                }, root_node=PROFILE_ROOT_NODE, file=fobj)
+
+        with open(os.path.join(self.configuration_dir, STATIC_OPTIONS_FILE), 'w') as f:
+            serialize(
+                dict((n, f.value) for n, f in  fields.iteritems() if f.static),
+                root_node=STATIC_OPTIONS_ROOT_NODE,
+                file=f
+            )
