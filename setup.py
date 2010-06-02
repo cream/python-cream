a6fae216fa2ff68417ba8ab160f684e7ba23547d
Sebastian Billaudelle <stein@cream-project.org>
2010-06-02 21:27:04 +0200

    Minor stuff.

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setuptools import setup, find_packages, Extension
+from distutils.core import setup
+from distutils.extension import Extension
 
 setup(
     name = 'Cream Libraries',
     version = '0.1',
-    packages = find_packages(),
+    packages = ['cream', 'cream.gui', 'cream.contrib', 'cream.config', 'cream.ipc', 'cream.util'],
     ext_modules=[Extension('cream.util.procname', ['cream/util/procnamemodule.c'])],
-    include_package_data = True,
     package_data={'cream.config': ['interface/*']}
     )
