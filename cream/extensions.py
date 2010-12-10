c87ea0af99388b5791f7eeeaa59f45a48a953688
Kristoffer Kleine <kris.kleine@yahoo.de>
2010-12-10 22:56:56 +0100

    handle multiple paths, refactored ExtensionManager._load

--- cream/extensions.py
+++ cream/extensions.py
@@ -48,39 +48,35 @@ class ExtensionManager(object):
         self.paths = paths
         self.interface = interface
 
-        self.extensions = ManifestDB(self.paths[0], type='org.cream.Extension') # TODO: multiple paths
+        self.extensions = ManifestDB(self.paths, type='org.cream.Extension')
 
     def load_all(self, interface=None, *args, **kwargs):
         return map(lambda ext: self._load(ext, interface),
                    self.extensions.by_name.itervalues())
 
     def load_by_name(self, name, interface=None, *args, **kwargs):
-
         ext = self.extensions.get_by_name(name)
         return self._load(ext, interface)
 
 
     def load_by_hash(self, hash, interface=None, *args, **kwargs):
-
         ext = self.extensions.get_by_hash(hash)
         return self._load(ext, interface)
 
 
     def _load(self, extension, interface=None, *args, **kwargs):
-        # '/foo/bar/extensions/myext.py' --> 'myext'
-        module_name = os.path.splitext(os.path.basename(extension['entry']))[0]
-        module_path = extension['path']
+        module_path = extension['entry'].replace('./', '')
 
         sys.path.append(module_path)
+        imp.load_module(
+            extension['name'],
+            open(module_path),
+            module_path,
+            ('.py', 'r', imp.PY_SOURCE)
+        )
 
-        # Importing the extension as a python module.
-        file, pathname, description = imp.find_module(module_name, [module_path])
-        mod = imp.load_module(module_name, file, pathname, description)
-
-        # Getting and instantiating the extension class.
-        cls = getattr(mod, EXTENSIONS[os.path.join(module_path, module_name + '.py')].__name__)
-        instance = cls(interface or self.interface, *args, **kwargs)
-
+        extension = EXTENSIONS[module_path]
+        instance = extension(interface or self.interface, *args, **kwargs)
         sys.path.remove(module_path)
 
         return instance
