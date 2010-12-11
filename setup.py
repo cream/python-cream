22d4549cecf730779faaeee72ab1c1edfc0df506
Kristoffer Kleine <kris.kleine@yahoo.de>
2010-12-11 23:36:59 +0100

    cleaned up setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
 from distutils.core import setup
-from distutils.extension import Extension
 
 setup(
     name = 'Cream Libraries',
     version = '0.1',
     packages = ['cream', 'cream.gui', 'cream.contrib', 'cream.config', 'cream.ipc', 'cream.util'],
     package_data={'cream.config': ['interface/*']}
-    )
+)
