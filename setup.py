3141ddf546ccd71722126f9e43d03e6c58cd25a6
Kristoffer Kleine <kris.kleine@yahoo.de>
2010-12-12 13:40:11 +0100

    extended setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from distutils.core import setup
 setup(
     name = 'cream-libs',
     version = '0.4',
+    author = 'The Cream Project (http://cream-project.org)',
+    url = 'http://github.com/cream/libs',
     packages = [
         'cream',
         'cream.gui',
