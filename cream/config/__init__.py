7ecfa005b557109ccdfd290fa5d7925e1cc86dea
Kristoffer Kleine <kris.kleine@yahoo.de>
2011-11-06 19:00:56 +0100

    Add value-changed signal to Configuration

--- cream/config/__init__.py
+++ cream/config/__init__.py
@@ -15,13 +15,21 @@
 # along with this library; if not, write to the Free Software Foundation, Inc.,
 # 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
+from gi.repository import GObject as gobject
+
 from cream.config.frontend import Frontend
 from cream.config.backend import Backend
 
 
-class Configuration(object):
+class Configuration(gobject.GObject):
+
+    __gsignals__ = {
+        'value-changed': (gobject.SignalFlags.RUN_LAST, None, (str, object)),
+    }
 
     def __init__(self, schema, static=None, hidden=None):
+
+        gobject.GObject.__init__(self)
 
         if static is None:
             static = []
@@ -34,7 +42,7 @@ class Configuration(object):
         self.frontend.connect('profile-selected', lambda f, p: self.backend.set_profile(p))
         self.frontend.connect('profile-added', lambda f, p: self.backend.add_profile(p))
         self.frontend.connect('profile-removed', lambda f, p: self.backend.remove_profile(p))
-        self.frontend.connect('value-changed', lambda f, k, v: self.backend.set_value(k, v))
+        self.frontend.connect('value-changed', self.on_value_changed)
 
 
     def show_dialog(self):
@@ -44,7 +52,6 @@ class Configuration(object):
     def save(self):
 
         self.backend.save()
-
 
 
     def __getattr__(self, name):
@@ -60,9 +67,16 @@ class Configuration(object):
         if 'frontend' in self.__dict__ and name in self.backend:
             return self.backend.set_value(name, value)
         else:
-            return object.__setattr__(self, name, value)
+            return gobject.GObject.__setattr__(self, name, value)
 
         raise AttributeError("No such attribute '{0}'".format(name))
+
+
+
+    def on_value_changed(self, frontend, key, value):
+
+        self.backend.set_value(key, value)
+        self.emit('value-changed', key, value)
 
 
 if __name__ == '__main__':
